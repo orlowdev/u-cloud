c53a9381be172610e88067c075066001180e0ff0
Sergei Orlov <hello@orlow.dev>
2021-09-21 15:28:55 +0300

    Rewrite with copyFileSync

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const { readFileSync, writeFileSync, statSync } = require('fs');
+const { copyFileSync, statSync } = require('fs');
 const { prop } = require('ramda');
 const { pipe, divideBy } = require('./src/utils');
 
 const sourceFilePath = './my.html';
 const outputFilePath = './выхлоп.html';
 
-const content = readFileSync(sourceFilePath, { encoding: 'utf-8' });
+copyFileSync(sourceFilePath, outputFilePath);
 
 const getFileSize = pipe(
     statSync,
     prop('size'),
     divideBy(1024),
     Math.round,
-)
-
-writeFileSync(outputFilePath, content);
+);
 
 console.log(
     `Copied ${getFileSize(sourceFilePath)}KB from ${sourceFilePath} to ${outputFilePath}`
