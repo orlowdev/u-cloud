1ba6de11eebb6286a93e22d9a788fdd29660dac9
Sergei Orlov <hello@orlow.dev>
2021-10-01 16:38:19 +0300

    Add saving file on a server

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { createReadStream, createWriteStream, stat } = require("fs");
 const { SingleBar, Presets } = require("cli-progress");
 const { createGzip } = require("zlib");
+const { request } = require("http");
 
 const bar = new SingleBar({}, Presets.shades_classic);
 
 const [sourceFilePath, outputFilePath] = process.argv.slice(2);
 
 const readStream = createReadStream(sourceFilePath);
-const writeStream = createWriteStream(outputFilePath);
 const gzipStream = createGzip();
 
 stat(sourceFilePath, (error, stat) => {
@@ -18,15 +18,52 @@ stat(sourceFilePath, (error, stat) => {
 
   bar.start(stat.size, 0);
 
-  readStream.pipe(
-    gzipStream
-      .on("data", (chunk) => {
-        writeStream.write(chunk);
+  if (process.argv.includes("--remote")) {
+    const requestOptions = {
+      method: "POST",
+      headers: { "file-path": outputFilePath },
+    };
 
-        bar.increment(chunk.length);
-      })
-      .on("close", () => {
-        bar.stop();
-      })
-  );
+    const req = request("http://localhost:8000", requestOptions, (res) => {
+      let responseBody = "";
+
+      res
+        .on("data", (chunk) => {
+          responseBody += chunk.toString();
+        })
+        .on("close", () => {
+          console.log(responseBody);
+        });
+    });
+
+    readStream.pipe(
+      gzipStream
+        .on("data", (chunk) => {
+          req.write(chunk);
+
+          bar.increment(chunk.length);
+        })
+        .on("close", () => {
+          req.end();
+
+          bar.update(stat.size);
+          bar.stop();
+        })
+    );
+  } else {
+    const writeStream = createWriteStream(outputFilePath);
+
+    readStream.pipe(
+      gzipStream
+        .on("data", (chunk) => {
+          writeStream.write(chunk);
+
+          bar.increment(chunk.length);
+        })
+        .on("close", () => {
+          bar.update(stat.size);
+          bar.stop();
+        })
+    );
+  }
 });
